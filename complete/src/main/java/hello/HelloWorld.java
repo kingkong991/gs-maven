e7378c2e14f98ebb208f2a8ba8898b17befddc69
Duy Nguyen <43160486+quyduy1992@users.noreply.github.com>
2022-01-07 14:55:53 +0700

    Update HelloWorld.java

--- complete/src/main/java/hello/HelloWorld.java
+++ complete/src/main/java/hello/HelloWorld.java
@@ -10,3 +10,19 @@ public class HelloWorld {
 		System.out.println(greeter.sayHello());
 	}
 }
+
+public static void switchCasePrimer() {
+    	int caseIndex = 0;
+    	switch (caseIndex) {
+        	case 0:
+            	System.out.println("Zero");
+        	case 1:
+            	System.out.println("One");
+            	break;
+        	case 2:
+            	System.out.println("Two");
+            	break;
+        	default:
+            	System.out.println("Default");
+    	}
+}
